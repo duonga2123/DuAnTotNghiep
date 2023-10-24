75399e32abccd5c365073782289d98a9fa3051c0
trafdual <you@example.com>
2023-10-24 17:23:23 +0700

    f

--- index.js
+++ index.js
@@ -418,8 +418,8 @@ app.post('/purchaseChapter/:userId/:chapterId', async (req, res) => {
 app.post('/chapters', async (req, res) => {
   try {
     const { mangaName, number, viporfree, image } = req.body;
+   
     const images=image.split('\n')
-    
 
     const manga = await Manga.findOne({ manganame: mangaName });
     if (!manga) {
