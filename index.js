d03d06a372f8480d5c4253ed4a173753d8982b51
trafdual <you@example.com>
2023-10-30 23:42:44 +0700

    f

--- index.js
+++ index.js
@@ -363,7 +363,7 @@ app.get('/mangachitiet/:mangaId/:userId', async (req, res) => {
 
     let isLiked = false;
     user.favoriteManga.forEach(favorite => {
-      if (favorite.mangaId._id === mangaId ) {
+      if (favorite.mangaId.toString()=== mangaId ) {
         isLiked = true;
         console.log(favorite.mangaId)
       }
