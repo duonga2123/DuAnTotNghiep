c1db6810da1219535dece149a8a1fb6fc00834ca
trafdual <you@example.com>
2023-11-03 23:45:48 +0700

    f

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ const paypal = require('paypal-rest-sdk');
 const cheerio = require('cheerio');
 const session = require('express-session');
 const redis = require('redis');
-const connectRedis = require('connect-redis');
+
+const RedisStore = require("connect-redis").default
 const Category = require('./models/CategoryModel')
-const multer = require('multer')
+
 const Manga = require('./models/MangaModel')
 const Chapter = require('./models/ChapterModel')
 const Payment = require('./models/PaymentModel')
@@ -19,7 +20,7 @@ const Handelbars = require('handlebars');
 const hbs = require('express-handlebars');
 const methodOverride = require('method-override');
 const path = require('path')
-const myId = new mongoose.Types.ObjectId();
+
 
 var app = express();
 
@@ -33,9 +34,7 @@ app.engine(".hbs", hbs.engine({
 app.set("view engine", ".hbs");
 app.set("views", path.join(__dirname, "views"));
 app.use(methodOverride('_method'));
-// const storage = multer.memoryStorage();
 
-// const upload = multer({ storage: storage });
 
 
 const uri = "mongodb+srv://totnghiepduan2023:MaNXmiIny7im1yjG@cluster0.tzx1qqh.mongodb.net/DuanTotNghiep?retryWrites=true&w=majority";
@@ -46,12 +45,15 @@ mongoose.connect(uri, {
 
 
 const redisClient = redis.createClient();
-const RedisStore = connectRedis(session);
+let redisStore = new RedisStore({
+  client: redisClient,
+  prefix: "myapp:",
+})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
-  store: new RedisStore({ client: redisClient }),
+  store: redisStore,
   secret: 'mysecretkey',
   resave: false,
   saveUninitialized: true,
