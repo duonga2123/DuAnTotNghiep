623e9321056da4f2a595aef01359f5f99d6856f2
trafdual <traz08102003@gmail.com>
2023-11-18 11:43:48 +0700

    f

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken');
 const paypal = require('paypal-rest-sdk');
 const cheerio = require('cheerio');
-const format  = require('date-fns');
 const session = require('express-session');
 const Category = require('./models/CategoryModel')
 const multer = require('multer')
@@ -170,7 +169,7 @@ app.get('/getbaiviet/:userId', async (req, res) => {
     const baiviet = await Baiviet.find({}).populate("userId", "username")
     const formattedBaiviet = baiviet.map(item => {
       const isLiked = user.favoriteBaiviet.some(favorite => favorite.baivietId.toString() === item._id.toString());
-      const formattedDate = format(item.date, 'dd/MM/yyyy');
+
       return {
         _id: item._id,
         userId: item.userId._id,
@@ -178,34 +177,31 @@ app.get('/getbaiviet/:userId', async (req, res) => {
         content: item.content,
         like: item.like,
         isLiked: isLiked,
-        date:formattedDate
+        date:item.date
       };
     });
     res.json(formattedBaiviet);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Đã xảy ra lỗi khi lấy thông tin bài viết', err });
+    res.status(500).json({ error: 'Đã xảy ra lỗi khi lấy thông tin bài viết' });
   }
 })
 
 app.get('/getbaiviet', async (req, res) => {
   try {
     const baiviet = await Baiviet.find({}).populate("userId", "username")
-    const formattedBaiviet = baiviet.map(item =>{
-      const formattedDate = format(item.date, 'dd/MM/yyyy');
-      return {
-        _id: item._id,
-        userId: item.userId._id,
-        username: item.userId.username,
-        content: item.content,
-        like: item.like,
-        date:formattedDate
-      };
-    });
+    const formattedBaiviet = baiviet.map(item => ({
+      _id: item._id,
+      userId: item.userId._id,
+      username: item.userId.username,
+      content: item.content,
+      like: item.like,
+      date:item.date
+    }));
     res.json(formattedBaiviet);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Đã xảy ra lỗi khi lấy thông tin bài viết',err });
+    res.status(500).json({ error: 'Đã xảy ra lỗi khi lấy thông tin bài viết' });
   }
 })
 
