e6af1b7330aace414ed00b79c8ad1d2e9b88b45f
trafdual <traz08102003@gmail.com>
2023-11-18 12:27:48 +0700

    f

--- models/BaiVietModel.js
+++ models/BaiVietModel.js
@@ -12,7 +12,7 @@ comment: [{
     cmt: { type: String }
   }],
   images: [String],
-  date: { type: Date, default: Date.now }
+  date: { type: Date, default: Date.now, required: true, expires: 60 }
 });
 
 const Baiviet = mongoose.model('baiviet', baivietSchema);
