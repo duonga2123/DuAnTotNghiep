58fb46563c780ee3549281c762f2e5e051302ece
trafdual <traz08102003@gmail.com>
2023-12-07 00:49:06 +0700

    f

--- index.js
+++ index.js
@@ -338,7 +338,7 @@ app.get('/getbaiviet', async (req, res) => {
           date: formatdatecmt
         };
       }));
-      const user = usersWithRoleNotVip.find(u =>u && u._id && u._id.toString() === item.userId._id.toString());
+      const user = usersWithRoleNotVip.find(u => u._id.toString() === item.userId._id.toString());
       if (user) {
         return {
           _id: item._id,
